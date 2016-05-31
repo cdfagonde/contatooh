afdb328ee22f1f0ca9b19f67a9bd63e5defa947b
cdfagonde <cdfagonde@hotmail.com>
2016-05-31 00:51:12 -0300

    Tratamento de variaveis de ambiente para o Openshift

--- config/passport.js
+++ config/passport.js
@@ -1,18 +1,25 @@
 // config/passport.js
 var passport = require( 'passport' );
 var GitHubStrategy = require( 'passport-github').Strategy;
+var mongoose = require( 'mongoose' );
+var config = require('./config')();
 
-var mongoose = require ( 'mongoose' );
-
+console.log("Configurações..");
+console.log(config);
 
 module.exports = function() {
 
+	// declarando uma variável para nos ajudar
+	var githubCallback = 'http://' + config.domain + ':' + config.port + '/auth/github/callback';
 	var Usuario = mongoose.model( 'Usuario' );
 
 	passport.use( new GitHubStrategy({
-		clientID : 'e0292448971dc933c588',
-		clientSecret : '588d0566191d2d8f3b1ac3d74926f2c294422219',
-		callbackURL : 'http://localhost:3000/auth/github/callback'
+		// clientID : 'e0292448971dc933c588',
+		// clientSecret : '588d0566191d2d8f3b1ac3d74926f2c294422219',
+		// callbackURL : 'http://localhost:3000/auth/github/callback'
+		clientID : config.clientID,
+		clientSecret : config.clientSecret,
+		callbackURL : githubCallback
 	}, function( accessToken , refreshToken , profile , done ) {
 
 		Usuario.findOrCreate(
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 // contatooh/server.js
 
+var config = require('./config/config')();
 var http = require( 'http' );
 var express = require ( 'express' );
 var app = require( './config/express' )();
 require( './config/passport' )();
-require( './config/database.js')( 'mongodb://localhost/contatooh' );
+// require( './config/database.js')( 'mongodb://localhost/contatooh' );
+require('./config/database')(config.db);
 
-http.createServer( app ).listen( app.get( 'port' ), function (){
-	console.log( 'Express Server escutando na porta ' + app.get( 'port' ));
+http.createServer( app ).listen( config.port, config.address, function (){
+	// console.log( 'Express Server escutando na porta ' + app.get( 'port' ));
+	console.log( 'Express Https Server '
+		+ config.address + ' (' + config.env + ') escutando na porta ' + config.port );
 });
